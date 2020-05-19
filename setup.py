198eaadff753f420873549c490fc80c4c1f2de92
adam <akabbeke@gmail.com>
2020-05-19 16:13:19 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='warhammer_stats',
-    version='0.1',
-    description='Stats tools ',
-    url='https://github.com/akabbeke/warhammer_stats',
-    author='Adam Kabbeke',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='warhammer_stats-akabbeke',
+    version="0.0.1",
+    author="Adam Kabbeke",
     author_email='akabbeke@gmail.com',
-    license='MIT',
-    packages=['warhammer_stats'],
-    zip_safe=False,
+    description="Warhammer 40k stats tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akabbeke/warhammer_stats",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     install_requires=[
         'numpy',
     ],
-    test_suite='nose.collector',
-    tests_require=['nose'],
     python_requires='>=3.6',
 )
